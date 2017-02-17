d4f9e46066d0bd15cbdc1beefdb1aa570ad8d6e4
Hudson Green <hudsongtv@outlook.com>
2017-02-16 21:56:20 -0800

    added roundfolders deb

---
+++ dev/depic/packagename/js/versionDetect.js
@@ -0,0 +1,39 @@
+var userOS;    // will either be iOS, Android or unknown
+var userOSver; // this is a string, use Number(userOSver) to convert
+var requiredOS = 10.0
+
+function getOS( )
+{
+  var ua = navigator.userAgent;
+  var uaindex;
+
+  // determine OS
+  if ( ua.match(/iPad/i) || ua.match(/iPod/i) || ua.match(/iPhone/i) )
+  {
+    userOS = 'iOS';
+    uaindex = ua.indexOf( 'OS ' );
+  }
+  else if ( ua.match(/Android/i) )
+  {
+    userOS = 'Android';
+    uaindex = ua.indexOf( 'Android ' );
+  }
+  else
+  {
+    userOS = 'unknown';
+  }
+
+  // determine version
+  if ( userOS === 'iOS'  &&  uaindex > -1 )
+  {
+    userOSver = ua.substr( uaindex + 3, 3 ).replace( '_', '.' );
+  }
+  else if ( userOS === 'Android'  &&  uaindex > -1 )
+  {
+    userOSver = ua.substr( uaindex + 8, 3 );
+  }
+  else
+  {
+    userOSver = 'unknown';
+  }
+}
