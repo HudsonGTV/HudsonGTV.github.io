8cd420f7a1e3abfdc625fd7670490c1171c4de41
Hudson Green <hudsongtv@outlook.com>
2017-02-18 22:46:03 -0800

    Update script.js

--- repo-layout/js/script.js
+++ repo-layout/js/script.js
@@ -40,3 +40,10 @@ addFooter = (function(year) {
 });
 
 addFooter(2017);
+
+function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = '//hudsongtv.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
