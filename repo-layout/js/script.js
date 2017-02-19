7d2de86ed402ace91cdbd8d446672c471e2fd037
Hudson Green <hudsongtv@outlook.com>
2017-02-18 17:58:37 -0800

    Theme fixes and added base version check scripts

--- repo-layout/js/script.js
+++ repo-layout/js/script.js
@@ -1,1 +1,16 @@
-/*(function(a){"use strict";navigator.userAgent.indexOf("Cydia")!=-1?(a.title=a.title.split(" \u00b7 ")[0],a.documentElement.classList.add("cydia")):a.documentElement.classList.remove("cydia","depiction")})(document)*/
+/*(function(a) {
+	"use strict";
+	navigator.userAgent.indexOf("Cydia")!=-1?(
+		a.title=a.title.split(" \u00b7 ")[0],
+		a.documentElement.classList.add("cydia")
+	):a.documentElement.classList.remove("cydia","depiction")
+})(document)*/
+
+var iOS = parseFloat(
+	('' + (/CPU.*OS ([0-9_]{1,5})|(CPU like).*AppleWebKit.*Mobile/i.exec(navigator.userAgent) || [0,''])[1])
+	.replace('undefined', '3_2').replace('_', '.').replace('_', '')
+) || false;
+
+document.getElementById('detected-version').innerHTML = 'iOS ' + iOS;
+
+document.addEventListener("touchstart", function(){}, true);
