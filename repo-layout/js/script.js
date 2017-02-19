56e297b769748f81577c09d1143e4635d9bbbac9
Hudson Green <hudsongtv@outlook.com>
2017-02-18 23:12:33 -0800

    Update script.js

--- repo-layout/js/script.js
+++ repo-layout/js/script.js
@@ -50,7 +50,7 @@ var disqus_config = function () {
 
 function() { // DON'T EDIT BELOW THIS LINE
 var d = document, s = d.createElement('script');
-s.src = '//hudsongtv.disqus.com/embed.js';
+s.src = 'http://hudsongtv.disqus.com/embed.js';
 s.setAttribute('data-timestamp', +new Date());
 (d.head || d.body).appendChild(s);
 })();
