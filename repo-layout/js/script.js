2091b8e7388ecc7a06a9eb8153915b87f75c8d35
Hudson Green <hudsongtv@outlook.com>
2017-02-19 17:32:03 -0800

    fixed bugs (added Google Analytics stuff)

--- repo-layout/js/script.js
+++ repo-layout/js/script.js
@@ -48,4 +48,19 @@ $('.link').attr('ontouchstart', '');
 $('.version-num').html(repoVersion);
 $('#inner-body-wrapper').after('<div id="page-bottom">HKG Repo ' + repoVersion + '</div>');
 
+// Google Analytics
+(function(i,s,o,g,r,a,m) {
+	i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)
+	},i[r].l=1*new Date();
+	a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];
+	a.async=1;
+	a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-92265922-1', 'auto');
+ga('send', 'pageview');
+
+
 
