f49c1bfc62b270b9f85a1b095a9d4df0f780aa26
Hudson Green <hudsongtv@outlook.com>
2017-02-21 15:38:46 -0800

    Update reverseList.js

--- js/reverseList.js
+++ js/reverseList.js
@@ -18,3 +18,12 @@ $('#toggle').click(function() {
 	}
 	
 });
+
+var lastTouchEnd = 0;
+document.documentElement.addEventListener('touchend', function (event) {
+  var now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
