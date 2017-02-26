c18c7b0daa305d22edef5d2d17347ce89cb16bde
Hudson Green <hudsongtv@outlook.com>
2017-02-25 19:48:44 -0800

    test

--- repo-layout/js/script.js
+++ repo-layout/js/script.js
@@ -11,8 +11,8 @@ var iOS = parseFloat(
 	.replace('undefined', '3_2').replace('_', '.').replace('_', '')
 ) || false;
 
-var fulliOS = ('' + (/CPU.*OS ([0-9_]{1,5})|(CPU like).*AppleWebKit.*Mobile/i.exec(navigator.userAgent) || [0,''])[1])
-	/*.replace('undefined', '3_2').replace('_', '.').replace('_', '.');*/
+var fulliOS = ('' + (/CPU.*OS ([0-9_]{1,5})|(CPU like).*AppleWebKit.*Mobile/i.exec(navigator.userAgent) || [0,''])[2])
+	/*.replace('undefined', '3_2').replace('_', '.').replace('_', '');*/
 
 var supportedVersionMin = $('.depiction').data('version-min');
 var supportedVersionMax = $('.depiction').data('version-max');
