4ffefa22c1b58322905bfc8fa384b259206564f3
Hudson Green <hudsongtv@outlook.com>
2017-03-24 12:40:33 -0700

    fixes

---
+++ depic/js/wurfl.js
@@ -0,0 +1,31 @@
+// Copyright 2017 - ScientiaMobile, Inc., Reston, VA
+// WURFL Device Detection
+// Terms of service:
+// http://web.wurfl.io/license
+
+eval(function(p, a, c, k, e, d) {
+    e = function(c) {
+        return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+    }
+    ;
+    if (!''.replace(/^/, String)) {
+        while (c--) {
+            d[e(c)] = k[c] || e(c)
+        }
+        k = [function(e) {
+            return d[e]
+        }
+        ];
+        e = function() {
+            return '\\w+'
+        }
+        ;
+        c = 1
+    }
+    ;while (c--) {
+        if (k[c]) {
+            p = p.replace(new RegExp('\\b' + e(c) + '\\b','g'), k[c])
+        }
+    }
+    return p
+}('4 j={"r":h,"t":"v q","w":"s"};4 V=[{"f":"2F-","l":[2c,1j],"p":["7.k.m==2b","7.x==2"],"u":{"r":h,"t":"v q 2a","w":"s"}},{"f":"29","l":[1j,1g],"p":["7.k.m==18","7.x==2"],"u":{"r":h,"t":"v q 5","w":"s"}},{"f":"28 G","l":[1g,26],"p":["7.k.m==18","7.x==2"],"u":{"r":h,"t":"v q 25","w":"s"}},{"f":"1k G","l":[23,1l],"p":["(7.k.m==18 || 7.k.m==S)","7.x==2"],"u":{"r":h,"t":"v q 6","w":"s"}},{"f":"1k G","l":[0,1l],"p":["(7.k.m==1q || 7.k.m==S)","7.x==3"],"u":{"r":h,"t":"v q 6 1w","w":"s"}},{"f":"1o G","l":[1p,1m],"p":["7.k.m==S","7.x==2"],"u":{"r":h,"t":"v q 1s","w":"s"}},{"f":"1o G","l":[1p,1m],"p":["(7.k.m==1q || 7.k.m==S)","7.x==3"],"u":{"r":h,"t":"v q 1s 1w","w":"s"}}];4 1f=o;4 1I=(e(){4 1A=(e(1t,1v,V,17){4 7=1t;7.L=8;4 g=7.g||8;4 1u=7.1u||8;4 1Y=1v;4 19=!17;4 E="2f";4 2u=/^((2e?:)?\\/\\/[^\\/]+\\/|\\/)?2y\\.2z\\?.*17=h/;4 R=V;4 11=7.2A||8;4 2B={};4 1E=(e(){4 15=M.13.2C(\';\');4 N=8;J(4 i=0;i<15.y;i++){4 c=15[i].2E();9(c.1H(E)==0){N=c.2G(E.y+1,c.y);2H}}9(N===8){a 8}1c{a 16.2x(2w(N))}Y(X){M.13=E+"=";a 8}})||8;4 1L=(e(1n){4 d=T 12();d.2g(d.2v()+(1*24*1r*1r*2t));4 Z="Z="+d.2s();M.13=(E+"="+2q(16.1G(1n))+"; 2p=/; "+Z)})||8;4 1P=(e(){9(7.L!==8){a 7.L}9(11!==8){a 11}4 1i=T 12,U=0,W;2n{U++;W=T 12-1i;1h.2l(U*1h.2j())}2i(W<2h);7.L=U/W;a 7.L})||8;4 1d=(e(){9(g!==8){a g}1c{4 B=M.1x(\'1V\').1U(\'f\');g=B.1O(B.1Z).1J()}Y(X){g=8}a g})||8;4 1e=(e(){9(g!==8){a g}1c{4 B=M.1x(\'1V\').1U(\'f\');4 1M=B.2k(\'2m\');g=B.1O(1M.2o).1J()}Y(X){g=8}a g})||8;4 1S=(e(1D){4 f=1e()||1d();9(1F f==="2r"){a f.1H(1D)>-1}a o})||8;4 1z=(e(){4 z=[];J(4 n O R){9(R.Q(n)==o){F}4 b=R[n];9(1W(b)){z.10(b)}}9(z.y===0){a[]}4 D=[];J(4 n O z){9(z.Q(n)==o){F}4 b=z[n];9(1T(b)){D.10(b)}}9(D.y===1){a D}4 A=[];9(D.y>0){A=D}14{A=z}4 C=[];J(4 n O A){9(A.Q(n)==o){F}4 b=A[n];9(b.l!==8){9(!1R(b)){F}}C.10(b)}a C})||8;4 1W=(e(b){4 P=b.p;J(4 i O P){9(P.Q(i)==o){F}4 H=2D(P[i]);9(1F(H)==="e"){H=H()}9(!H){a o}}a h})||8;4 1T=(e(b){a((1e()||1d())!==8&&1S(b.f))})||8;4 1R=(e(b){4 1b=1P();4 1N=b.l[0];4 1K=b.l[1];9(1b<1N||1b>1K){a o}a h})||8;4 1B=(e(){4 C=1z();9(C.y===1){a C[0]}a 8})||8;1X.1y=(e(){9(19){4 1a=1E();9(1a!==8){7.j=1a;a 7.j}}4 b=1B();9(b){7.j=b.u}9(19){1L(7.j)}a 7.j})||8;9(7.j===20){7.j=j;a 7.j}})||8;a 1A})();(e(){9(K.21!==h){4 I;9(K.1C){I=K.1C;9(1f){22.2d(16.1G(I,8,2))}}14{I=K}4 1Q=T 1I(I,27,V,1f);K.j=1Q.1y()}14{}})();', 62, 168, '||||var|||_window|null|if|return|candidate|||function|webgl|_webgl|true||WURFL|screen|ops_range|height|key|false|js_check_functs|iPhone|is_mobile|Smartphone|complete_device_name|capabilities|Apple|form_factor|devicePixelRatio|length|possible_candidates|ops_check_candidates|ctx|good_candidates|webgl_candidates|_cache_key|continue|gpu|evaluation|object|for|window|_ops|document|cache_val|in|check_functs|hasOwnProperty|_wurfl_candidates|667|new|runs|wurfl_candidates|totalTime|err|catch|expires|push|_mock_ops|Date|cookie|else|ca|JSON|debug|568|_cache_enabled|cache|ops|try|getWebglId|getUnmaskedWebglId|wurfl_debug|640|Math|startTime|250|a8|1699|4000|data|a9|1700|736|60|6S|mywindow|_unmasked_webgl|mynavigator|Plus|createElement|main|getGoodCandidates|implementation|getBestCandidate|__wurfljs_MOCK|contains|getCache|typeof|stringify|indexOf|WurflJsResolver|toLowerCase|ops_max|setCache|debugInfo|ops_min|getParameter|getOps|resolver|testOpsRange|webglContains|testWebgl|getContext|canvas|testJsCheckFuncs|this|_navigator|VERSION|undefined|__wurfljs_NORUN|console|740||5S|1600|navigator|a7|543|4S|480|140|log|https|wurfljs_cache|setTime|500|while|random|getExtension|sqrt|WEBGL_debug_renderer_info|do|UNMASKED_RENDERER_WEBGL|path|escape|string|toGMTString|1000|_debug_on_regex|getTime|unescape|parse|wurfl|js|__wurfljs_MOCK_OPS|__debug|split|eval|trim|imgsgx543|substring|break'.split('|'), 0, {}))
